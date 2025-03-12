5b1e43e4417669bf5748270bbba368f4c62b60f9
yamadapinz <146986119+yamadaguilherme@users.noreply.github.com>
2025-03-12 19:12:50 -0300

    Delete secao3aula64.py

--- secao3aula64.py
+++
@@ -1,8 +0,0 @@
-nome = 'Guilherme Yamada'
-tamanho_nome = 2*len(nome)
-escada = ''
-contador = 0
-while len(escada) != tamanho_nome:
-    escada += f'{nome[contador]} '
-    contador += 1
-    print(escada)
